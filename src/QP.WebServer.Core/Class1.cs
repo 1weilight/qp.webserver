c9a2a45727aca602ed4fc999be44380224140a11
1weilight <m6497485@126.com>
2023-07-31 10:52:46 +0800

    first commit

---
+++ src/QP.WebServer.Core/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace QP.WebServer.Core
+{
+    public class WebServer : IServer
+    {
+        public IFeatureCollection Features => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            ///to understand
+            var cancelledTokenSource = new CancellationTokenSource();
+            cancelledTokenSource.Cancel();
+            StopAsync(cancelledTokenSource.Token).GetAwaiter().GetResult();
+        }
+
+        public Task StartAsync<TContext>(IHttpApplication<TContext> application, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
